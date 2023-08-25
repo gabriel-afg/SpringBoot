746405bd4f953ef58fcbcc9fb82018025eccf7bf
Gabriel Augusto <Gabriel.afg182@gmail.com>
2023-08-25 11:19:58 -0300

    Category

---
+++ src/main/java/com/springbootproject/project/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.springbootproject.project.services;
+
+import com.springbootproject.project.entities.Category;
+import com.springbootproject.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
